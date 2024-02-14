daf5b48b83c3a1adf5ccce4e0ef3a058e2b9f827
Ananay Ojha <76782360+git-of-aj@users.noreply.github.com>
2024-02-14 15:47:12 +0530

    Create new_table.sql

---
+++ new_table.sql
@@ -0,0 +1,26 @@
+\c tbt_14_feb;
+
+CREATE TABLE login (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+INSERT INTO login (username, password, email) VALUES
+('user1', 'password1', 'user1@example.com'),
+('user2', 'password2', 'user2@example.com'),
+('user3', 'password3', 'user3@example.com'),
+('user4', 'password4', 'user4@example.com'),
+('user5', 'password5', 'user5@example.com'),
+('user6', 'password6', 'user6@example.com'),
+('user7', 'password7', 'user7@example.com'),
+('user8', 'password8', 'user8@example.com'),
+('user9', 'password9', 'user9@example.com'),
+('user10', 'password10', 'user10@example.com');
+
+-- show all tables
+ \dt
+
+-- fetch everything
+select * from login;
